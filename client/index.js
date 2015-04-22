096531136dc9371043881ec9ccff48af911d87a2
Chris Oyler <christopher.oyler@gmail.com>
2015-04-21 23:59:47 -0700

    search by year works

--- client/index.js
+++ client/index.js
@@ -1,3 +1,15 @@
 'use strict';
 
-angular.module('poseidon', ['firebase']);
+angular.module('f1Feeder', [])
+.run(['$rootScope', function($rootScope){
+  $rootScope.driverInfo=[];
+}])
+.controller('f1Controller', ['$rootScope', '$scope', '$http', function($rootScope, $scope, $http){
+  $scope.findYear = function(){
+    var year = $scope.year;
+    $http.get('http://ergast.com/api/f1/' + year + '/driverstandings.json')
+    .then(function(response){
+      $rootScope.driverInfo = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    });
+  };
+}]);
